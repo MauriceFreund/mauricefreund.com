2c072dea0d10021917f27cd9e2e30096dab71bc6
MauriceFreund <maurice-freund@gmx.de>
2021-02-04 23:24:09 +0100

    Project Setup

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    purge: ["./src/**/*.html", "./src/**/*.js"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                one: {
+                    lt: "#EF6B81",
+                    md: "#E83151",
+                    dk: "#B4142F",
+                },
+                two: {
+                    lt: "#0BC2FF",
+                    md: "#007EA7",
+                    dk: "#005A78",
+                },
+                three: {
+                    lt: "#FFBF49",
+                    md: "#FFA400",
+                    dk: "#B67600",
+                },
+            },
+        },
+    },
+    variants: {
+        extend: {
+            backgroundColor: ["active"],
+        },
+    },
+    plugins: [],
+};
